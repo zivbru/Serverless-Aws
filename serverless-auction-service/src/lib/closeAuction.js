e40f45cd8af9b357d59065bf8f02cb892585222b
zivbru <zivbru123@gmail.com>
2020-10-21 14:26:31 +0300

    Added sending mail logic to auction service to notify seller and buyer by mail when auction is closed

--- serverless-auction-service/src/lib/closeAuction.js
+++ serverless-auction-service/src/lib/closeAuction.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 
 const dynmodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
   const params = {
@@ -15,6 +16,46 @@ export async function closeAuction(auction) {
     },
   };
 
-  const result = await dynmodb.update(params).promise();
-  return result;
+  await dynmodb.update(params).promise();
+
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  if (amount === 0) {
+    await sqs
+      .sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+          subject: 'no bids on your auction item :(',
+          recipient: seller,
+          body: `Oh no! Your item "${title} ddon't get any bids. Better luck next time`,
+        }),
+      })
+      .promise();
+    return;
+  }
+
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'Your item has beed sold!',
+        recipient: seller,
+        body: `Woooho! Your item "${title} has been sold for $${amount}"`,
+      }),
+    })
+    .promise();
+
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'You won an auction!',
+        recipient: bidder,
+        body: `What a great deal! you got yourself a "${title}" for $${amount}`,
+      }),
+    })
+    .promise();
+
+  await Promise.all([notifyBidder, notifySeller]);
 }
--- serverless-auction-service/src/lib/getEndedAuctions.js
+++ serverless-auction-service/src/lib/getEndedAuctions.js
@@ -4,12 +4,11 @@ const dynmodb = new AWS.DynamoDB.DocumentClient();
 
 export async function getEndedAuctions() {
   const now = new Date();
-
+  // beacuse status is a reserves word we need the changes it so in run time the word #status will be changed to status
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     IndexName: 'statusAndEndDate',
-    // beacuse status is a reserves word we need the changes it so in run time the word #status will be changed to status
-    keyConditionExpression: '#status = :status AND endingAt <= :now',
+    KeyConditionExpression: '#status = :status AND endingAt <= :now',
     ExpressionAttributeValues: {
       ':status': 'OPEN',
       ':now': now.toISOString(),
